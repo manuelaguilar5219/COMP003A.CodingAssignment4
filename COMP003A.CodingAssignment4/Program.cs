3d07a846dc0d3e92206e801e0010c664ca073cc0
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2025-02-06 19:00:14 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment4/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace COMP003A.CodingAssignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] productNames = new string[10];
+            int[] productQuantities = new int[10];
+            int count = 0;
+
+            Console.WriteLine("Welcome to the Inventory Management System!");
+
+            int userChoice1;
+            int userChoice2;
+
+            Console.WriteLine("Choose data storage type");
+            Console.WriteLine("1. Arrays");
+            Console.WriteLine("2. List");
+            Console.WriteLine("Enter your choice");
+            userChoice1 = int.Parse(Console.ReadLine());
+
+            if (userChoice1 == 1)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Inventory Management System Menu:");
+                    Console.WriteLine("1. Add a Product");
+                    Console.WriteLine("2. Update Product quantity");
+                    Console.WriteLine("3. View Inventory Summary");
+                    Console.WriteLine("4. Exit");
+                    Console.WriteLine("Enter your choice");
+                    userChoice2 = int.Parse(Console.ReadLine());
+
+                    switch (userChoice2)
+                    {
+                        case 1:
+                            if (count < productNames.Length)
+                            {
+                                Console.WriteLine("Enter product name: ");
+                                productNames[count] = Console.ReadLine();
+                                Console.WriteLine("Enter product quantity: ");
+                                productQuantities[count] = int.Parse(Console.ReadLine());
+                                count++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Inventory is full!");
+                            }
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter the product name: ");
+                            int index = Array.IndexOf(productNames, Console.ReadLine());
+                            if (index != 1)
+                            {
+                                Console.WriteLine("Enter new product quantity: ");
+                                productQuantities[index] = int.Parse(Console.ReadLine());
+                            }
+                            else
+                            {
+                                Console.WriteLine("Product not found");
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
